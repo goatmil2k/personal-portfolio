c560c372a403a32f367073a305db6170bf63cb8d
goatmil2k <krishnakaflay200@gmail.com>
2024-04-17 20:50:50 +0700

    changes image path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <>
             <div className="footer">
-                <img className="profile-photo" src="../public/1644995893247.jpg"/>
+                <img className="profile-photo" src="/1644995893247.jpg"/>
                 <i className="fab fa-github" style={{ fontSize: "30px",}}></i><p style={{ marginLeft: "-2rem"}}><a href="https://www.github.com/goatmil2k">Git Hub</a></p>
                 <i class="fab fa-linkedin" style={{ fontSize: "30px"}}></i><p style={{ marginLeft: "-2rem"}}><a href="https://www.linkedin.com/in/krishna-kaflay-65715a232/">LinkedIn</a></p>
             </div>
